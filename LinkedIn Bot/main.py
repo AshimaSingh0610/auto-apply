329381ede854a107504cee286345b8e827226ebb
Ashima <ashimasingh0610@gmail.com>
2024-08-15 08:22:36 +0530

    Updated files

--- LinkedIn Bot/main.py
+++ LinkedIn Bot/main.py
@@ -3,19 +3,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 # Path to the ChromeDriver executable
-chrome_driver_path = "E:/Applications/chromedriver-win64/chromedriver.exe"
+chrome_driver_path = "E:\Applications\chromedriver-win64\chromedriver-win64\chromedriver.exe"
 
 # Create a Service object
 service = Service(executable_path=chrome_driver_path)
 
-# Optional: Set Chrome options
-chrome_options = Options()
-# chrome_options.add_argument("--headless")  # Uncomment to run in headless mode
 
 # Initialize the ChromeDriver with Service and Options
-driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome(service=service)
 
 # Open LinkedIn
 driver.get("https://www.linkedin.com")
+
+
+
+driver.close()
 
 
